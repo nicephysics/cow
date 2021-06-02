dc2990585c5cc33b8fce04718fea1aad7160ce4a
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-02 18:48:49 +0800

    Update script_1.js

--- one/ladder/script_1.js
+++ one/ladder/script_1.js
@@ -55,6 +55,10 @@ function hint1() {
     p.id = "text_hint_1"
     p.innerHTML = "Hint 1: Draw a diagram and include all the forces acting on the ladder."
     document.getElementById("footer").appendChild(p)
+    document.documentElement.scrollTo({
+      bottom: 0,
+      behavior: "smooth"
+    })
   }
 }
 
@@ -66,6 +70,10 @@ function hint2() {
     p.id = "text_hint_2"
     p.innerHTML = "Hint 2: There are two forces acting on the ladder by the ground! Don't forget the weight of the ladder."
     document.getElementById("footer").appendChild(p)
+    document.documentElement.scrollTo({
+      bottom: 0,
+      behavior: "smooth"
+    })
   }
 }
 
@@ -77,6 +85,10 @@ function hint3() {
     p.id = "text_hint_3"
     p.innerHTML = "Hint 3: Use Fnet = 0 (upward forces = downward forces) to find the normal force on the ladder by the ground."
     document.getElementById("footer").appendChild(p)
+    document.documentElement.scrollTo({
+      bottom: 0,
+      behavior: "smooth"
+    })
   }
 }
 
@@ -88,6 +100,10 @@ function hint4() {
     p.id = "text_hint_4"
     p.innerHTML = "Hint 4: Take moments about the pivot at the top of the ladder, because you don't know the normal force by the wall on the ladder."
     document.getElementById("footer").appendChild(p)
+    document.documentElement.scrollTo({
+      bottom: 0,
+      behavior: "smooth"
+    })
   }
 }
 
@@ -99,5 +115,9 @@ function hint5() {
     p.id = "text_hint_5"
     p.innerHTML = "Hint 5: Use the principle of moments to calculate the answer! Remember that the distance required is the distance from the bottom of the ladder."
     document.getElementById("footer").appendChild(p)
+    document.documentElement.scrollTo({
+      bottom: 0,
+      behavior: "smooth"
+    })
   }
 }
