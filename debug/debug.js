f710cd97c09e188b5e5aceef83390b2b96c02509
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-04 18:42:08 +0800

    Update debug.js

--- debug/debug.js
+++ debug/debug.js
@@ -1,14 +1,22 @@
-document.getElementById("checkbox_debug_map").addEventListener("change", function() {
+var debug_map = document.getElementById("checkbox_debug_map")
+var music_play = document.getElementById("checkbox_play_music")
+
+debug_map.addEventListener("change", function() {
   if (this.checked) {
     localStorage.setItem("debug_map", "true")
   } else {
     localStorage.setItem("debug_map", "false")
   }
 })
-document.getElementById("checkbox_play_music").addEventListener("change", function() {
+music_play.addEventListener("change", function() {
   if (this.checked) {
     localStorage.setItem("music_play", "true")
   } else {
     localStorage.setItem("music_play", "false")
   }
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+  debug_map.checked = (localStorage.getItem("debug_map") === "true")
+  music_play.checked = (localStorage.getItem("music_play") === "true")
 })
