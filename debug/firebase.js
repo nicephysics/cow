e6580a7e6e81b1afe6f6054b711d84fe4685c0a3
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-05 10:48:29 +0800

    Update firebase.js

--- debug/firebase.js
+++ debug/firebase.js
@@ -1,5 +1,40 @@
-var database = firebase.database() // yay
+var database = function() {
+  return firebase.database() // yay
+}
 
-database.ref("test").set("Hello World!")
+// set
+database().ref("test").set("Hello World!")
 
-console.log(database)
+// read listener
+database().ref("test").on('value', (snapshot) => {
+  const value = snapshot.val();
+  console.log("listener", value)
+})
+
+// read one using get()
+const root = database().ref()
+root.child("path").child("one").get().then((snapshot) => {
+  if (snapshot.exists()) {
+    const value = snapshot.val()
+    console.log("get", value)
+  } else {
+    console.log("No data available!!!")
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+// read once using observer
+firebase.database().ref("/path/one").once('value').then((snapshot) => {
+  const value = snapshot.val()
+  console.log("observer", value)
+  // ...
+});
+
+// update
+var updates = {}
+updates["path/one"] = "1"
+updates["path/two"] = "2"
+database().ref().update(updates)
+
+console.log(database())
